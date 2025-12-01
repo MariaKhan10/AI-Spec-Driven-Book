1d96d450600e6b3a072cec3a46a74224d62fe996
Maria Khan <mariapervaiz40@gmail.com>
2025-12-01 23:11:30 +0500

    Deploy website - based on 53812f684fa19c29396bff4ed5a6c9273428d398

--- rag-chatbot-backend/vector_store.py
+++
@@ -1,8 +0,0 @@
-from qdrant_client import QdrantClient
-import os
-
-QDRANT_HOST = os.getenv('QDRANT_HOST')
-QDRANT_API_KEY = os.getenv('QDRANT_API_KEY')
-
-qdrant = QdrantClient(url=QDRANT_HOST, api_key=QDRANT_API_KEY)
-COLLECTION_NAME = "book_embeddings"
